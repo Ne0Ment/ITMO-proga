9659c2d628d9111cf58e54cec120077353c75f1d
NeoMent <16ilyamikheev@gmail.com>
2022-09-22 08:08:13 +0300

    added table of contents to lab1

--- lab1/Lab1.java
+++ lab1/Lab1.java
@@ -48,14 +48,14 @@ public class Lab1 {
         for (int i=0; i<b[0].length; i++) {
             int maxW = -1;
             for (int j=0; j<b.length; j++) {
-                maxW = (int)Math.max(maxW, String.format("%.3f", b[j][i]).length());
+                maxW = (int)Math.max(maxW, String.format("%.2f", b[j][i]).length());
             }
             colWidths[i] = maxW;
         }
 
         for (int i=0; i<b.length; i++) {
             for (int j=0; j<b[0].length; j++) {
-                String curNum = String.format("%.3f ", b[i][j]);
+                String curNum = String.format("%.2f ", b[i][j]);
                 System.out.print(" ".repeat(colWidths[j]-curNum.length()+1) + curNum);
             }
             System.out.print("\n");
